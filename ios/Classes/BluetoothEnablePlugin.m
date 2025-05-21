7b0ec9e23f3784b09196828b9d257878f9efe3fe
Dev-GauravKumar <gauravkumar869999@gmail.com>
2025-05-21 13:16:07 +0530

    plugin name changed

--- ios/Classes/BluetoothEnablePlugin.m
+++ ios/Classes/BluetoothEnablePlugin.m
@@ -1,11 +1,12 @@
 #import "BluetoothEnablePlugin.h"
-#if __has_include(<bluetooth_enable_fork/bluetooth_enable_fork-Swift.h>)
-#import <bluetooth_enable_fork/bluetooth_enable_fork-Swift.h>
+
+#if __has_include(<bluetooth_enable_plus/bluetooth_enable_plus-Swift.h>)
+#import <bluetooth_enable_plus/bluetooth_enable_plus-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "bluetooth_enable_fork-Swift.h"
+#import "bluetooth_enable_plus-Swift.h"
 #endif
 
 @implementation BluetoothEnablePlugin
